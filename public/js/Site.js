794c14fb92e5fa81e8fa8a2e642d9ef8acce9fc1
Craig Schwartz <cas5cs@virginia.edu>
2016-02-28 22:28:07 -0500

    Styling for navbar

--- public/js/Site.js
+++ public/js/Site.js
@@ -1,29 +1,35 @@
 var NavItem = React.createClass({
-  onNavItemSelected: function(e) {
+  hanldeClick: function(e) {
     e.preventDefault();
     this.props.onNavItemSelected(this.props.content);
   },
   render: function() {
+    var style = {
+      'background-color': '#f0f0f0'
+    }
+
+    if(this.props.content === this.props.selectedNavItem){
+      style = {
+        'background-color': '#87cefa'
+      }
+    }
+
     return (
-      <div className="navItem">
-        <button onClick={this.onNavItemSelected}>{this.props.content}</button>
+      <div className='navItem noselect' onClick={this.hanldeClick} style={style}>
+        {this.props.content}
       </div>
     );
   }
 });
 
 var NavBar = React.createClass({
-  getInitialState: function() {
-    return {
-      selectedPanel: 'Home'
-    };
-  },
   render: function() {
     return (
-      <div className="navBar">
-        <NavItem content='Home' onNavItemSelected={this.props.onNavChanged} />
-        <NavItem content='About' onNavItemSelected={this.props.onNavChanged} />
-        <NavItem content='Contact' onNavItemSelected={this.props.onNavChanged} />
+      <div id='navBar'>
+        <img id='navBarLogo' src='assets/images/logo.png' />
+        <NavItem content='Home' selectedNavItem={this.props.selectedItem} onNavItemSelected={this.props.onNavChanged} />
+        <NavItem content='About' selectedNavItem={this.props.selectedItem} onNavItemSelected={this.props.onNavChanged} />
+        <NavItem content='Contact' selectedNavItem={this.props.selectedItem} onNavItemSelected={this.props.onNavChanged} />
       </div>
     );
   }
@@ -32,8 +38,8 @@ var NavBar = React.createClass({
 var MainContainer = React.createClass({
   render: function() {
     return (
-      <div className="mainContainer">
-        <h1>{this.props.selectedPanel}</h1>
+      <div id='mainContainer'>
+        <h1 id='pageTitle'>{this.props.selectedPanel}</h1>
       </div>
     );
   }
@@ -52,7 +58,7 @@ var Site = React.createClass({
   },
   render: function() {
     return (
-      <div className="site">
+      <div className='site'>
         <NavBar selectedItem={this.state.selectedPanel} onNavChanged={this.onNavChanged} />
         <MainContainer selectedPanel={this.state.selectedPanel} />
       </div>
