3a88a71542e1e4309cc240d2d619b58cfa636f57
cschwartz525 <crsc@trustpilot.com>
2016-03-24 15:07:51 -0400

    Added work history page

---
+++ src/react/components/resume/workHistory.jsx
@@ -0,0 +1,50 @@
+var React = require('react');
+
+var WorkHistory = React.createClass({
+  render: function() {
+    return (
+      <div className="workHistory">
+        <div className="workHistoryHeader">
+          <div className="left">
+            <div>
+              <a href={this.props.webiste}>
+                {this.props.companyName}
+              </a>
+            </div>
+            <div>
+              {this.props.jobTitle}
+            </div>
+          </div>
+          <div className="right">
+            <div className="companyLogo"></div>
+          </div>
+        </div>
+
+        <div className="workHistorySummary">
+          <div className="left">
+            <div>
+              {this.props.department}
+            </div>
+            <div>
+              {this.props.location}
+            </div>
+            <div>
+              {this.props.startDate} - {this.props.endDate}
+            </div>
+            <div>
+              <a href={this.props.webiste}>
+                {this.props.website}
+              </a>
+            </div>
+          </div>
+          <div className="right">
+            <p>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = WorkHistory;
