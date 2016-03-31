b019646574377e0d8e870d7cf80c433b41fd9bba
cschwartz525 <crsc@trustpilot.com>
2016-03-31 17:24:56 -0400

    Added jsx file for skills

---
+++ src/react/components/resume/skill.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+
+var Skill = React.createClass({
+  render: function() {
+    return (
+      <div className="skill">
+        <div className="skillHeader">
+          <div className="left">
+          </div>
+          <div className="right">
+          </div>
+        </div>
+        <div className="skillSummary">
+          <div className="left">
+          </div>
+          <div className="right">
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Skill;
