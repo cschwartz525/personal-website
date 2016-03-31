c7932e66e5424ce383987059a05dc7790dd7a30a
cschwartz525 <crsc@trustpilot.com>
2016-03-31 17:14:34 -0400

    Moved jobs section to map function

--- src/react/components/resume/resumePage.jsx
+++ src/react/components/resume/resumePage.jsx
@@ -22,42 +22,28 @@ var ResumePage = React.createClass({
   },
 
   render: function() {
+    var jobs = this.state.jobs.map(function(job) {
+      return (
+        <WorkHistory
+          companyName={job.companyName}
+          jobTitle={job.jobTitle}
+          department={job.department}
+          location={job.location}
+          startDate={job.startDate}
+          endDate={job.endDate}
+          website={job.website}
+          logo={job.logo}
+        />
+      );
+    });
+
     return (
       <div id="mainContent">
         <h1 className="pageTitle">Resume</h1>
 
         <h2 className="pageSubtitle">Work Experience</h2>
         <div id="previousJobs">
-          <WorkHistory
-            companyName="Trustpilot"
-            jobTitle="Software Developer"
-            department="Commercial Growth Team"
-            location="New York, NY"
-            startDate="2016"
-            endDate="Present"
-            website="https://www.trustpilot.com/"
-            logo="trustpilot.png"
-          />
-          <WorkHistory
-            companyName="Citigroup"
-            jobTitle="Assitant Vice President - Software Devleopment"
-            department="ICG Technology"
-            location="New York, NY"
-            startDate="2013"
-            endDate="2016"
-            website="http://www.citigroup.com/citi/"
-            logo="citi.png"
-          />
-          <WorkHistory
-            companyName="The Lloyd Group"
-            jobTitle="Operations Intern"
-            department="Summer Internship"
-            location="New York, NY"
-            startDate="2012"
-            endDate="2012"
-            website="http://www.lloydgroup.com/"
-            logo="lloyd.png"
-          />
+          {jobs}
         </div>
 
         <h2 className="pageSubtitle">Education</h2>
