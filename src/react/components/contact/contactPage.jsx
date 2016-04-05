a104b8b055eb537bd0eb321887ebc8533e452573
cschwartz525 <crsc@trustpilot.com>
2016-04-05 17:09:35 -0400

    Added EmailForm

--- src/react/components/contact/contactPage.jsx
+++ src/react/components/contact/contactPage.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
+var EmailForm = require('./emailForm');
 
 var ContactPage = React.createClass({
   render: function() {
     return (
       <div id="mainContent">
         <h1 className="pageTitle">Contact</h1>
+        <EmailForm />
         <div id="contactList">
           <div className="contactItem"><a href="mailto:craigschwartz525@gmail.com">craigschwartz525@gmail.com</a></div>
           <div className="contactItem">914-373-1348</div>
---
+++ src/react/components/contact/emailForm.jsx
@@ -0,0 +1,39 @@
+var React = require('react');
+
+var EmailForm = React.createClass({
+  render: function() {
+    return (
+      <div id="emailForm">
+        <h2>Contact Me</h2>
+        <form
+            action="/api/sendEmail"
+            method="post"
+            enctype="text/plain"
+        >
+          <input
+              type="text"
+              name="sender"
+              placeholder="Your email"
+          />
+          <br />
+          <input
+              type="text"
+              name="subject"
+              placeholder="Subject"
+          />
+          <br />
+          <textarea
+              cols="100"
+              name="message"
+              placeholder="..."
+              rows="20"
+          />
+          <input type="submit" value="Send" />
+          <input type="reset" value="Reset" />
+        </form>
+      </div>
+    );
+  }
+});
+
+module.exports = EmailForm;
