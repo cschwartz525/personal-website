00caf41cd92da942aac073591fb91e6d855bd01f
Craig Schwartz <craigschwartz525@gmail.com>
2016-06-03 15:02:27 -0400

    Fixed contact items

--- src/react/components/contact/contactPage.jsx
+++ src/react/components/contact/contactPage.jsx
@@ -15,8 +15,9 @@ var ContactPage = React.createClass({
         <h2 className="pageSubtitle">Additional Contact Info</h2>
         <div className="pageContent">
           <div id="contactList">
-            <div className="contactItem"><a href="mailto:craigschwartz525@gmail.com">craigschwartz525@gmail.com</a></div>
-            <div className="contactItem">914-373-1348</div>
+            <div className="contactItem"><b>Email:</b> <a href="mailto:craigschwartz525@gmail.com">craigschwartz525@gmail.com</a></div>
+            <div className="contactItem"><b>Phone:</b> <a href="tel:914-373-1348">914-373-1348</a></div>
+            <div className="contactItem"><b>LinkedIn:</b> <a href="https://www.linkedin.com/in/cschwartz525">cschwartz525</a></div>
           </div>
         </div>
       </div>
