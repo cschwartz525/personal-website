61720378582f58a3f1650f9acb3792880684e946
Craig Schwartz <craigschwartz525@gmail.com>
2016-08-08 22:42:15 -0400

    Added class textLink

--- src/react/components/contact/contactPage.jsx
+++ src/react/components/contact/contactPage.jsx
@@ -16,9 +16,9 @@ var ContactPage = React.createClass({
         <h2 className="pageSubtitle">Additional Contact Info</h2>
         <div className="pageContent">
           <div id="contactList">
-            <div className="contactItem"><a href="mailto:craigschwartz525@gmail.com"><i className="fa fa-envelope" /> craigschwartz525@gmail.com</a></div>
-            <div className="contactItem"><a href="tel:914-373-1348"><i className="fa fa-phone" /> 914-373-1348</a></div>
-            <div className="contactItem"><a href="https://www.linkedin.com/in/cschwartz525"><i className="fa fa-linkedin" /> cschwartz525</a></div>
+            <div className="contactItem"><a className="textLink" href="mailto:craigschwartz525@gmail.com"><i className="fa fa-envelope" /> craigschwartz525@gmail.com</a></div>
+            <div className="contactItem"><a className="textLink" href="tel:914-373-1348"><i className="fa fa-phone" /> 914-373-1348</a></div>
+            <div className="contactItem"><a className="textLink" href="https://www.linkedin.com/in/cschwartz525"><i className="fa fa-linkedin" /> cschwartz525</a></div>
           </div>
         </div>
       </div>
