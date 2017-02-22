aea30142e999412138c98f908e44d95a73281002
Craig Schwartz <craigschwartz525@gmail.com>
2017-02-21 21:19:16 -0500

    Improved email validation

--- src/react/components/contact/emailForm.jsx
+++ src/react/components/contact/emailForm.jsx
@@ -3,7 +3,6 @@ var React = require('react');
 var EmailForm = React.createClass({
   displayName: 'EmailForm',
   render: function() {
-    var emailRegex = "^[a-zA-Z0-9-_.]+@[a-zA-Z0-9-_.]+\.(com|org|net|edu|info|int|gov|mil|[a-z]{2}|co.[a-z]{2})$";
     return (
       <div id="emailForm">
         <form
@@ -13,8 +12,8 @@ var EmailForm = React.createClass({
         >
           <input
               className="formText"
+              id="contact-form-email"
               name="sender"
-              pattern={emailRegex}
               placeholder="Your email"
               size="50"
               type="text"
@@ -35,7 +34,7 @@ var EmailForm = React.createClass({
               rows="20"
           />
           <br />
-          <input className="formButton" type="submit" value="Send" />
+          <input className="formButton" disabled id="contact-form-submit" type="submit" value="Send" />
           <input className="formButton" type="reset" value="Reset" />
         </form>
       </div>
