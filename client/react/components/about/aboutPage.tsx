768c0729dc6f4761b28d012139c870a13c6c5176
Craig Schwartz <craigschwartz525@gmail.com>
2021-06-07 20:13:48 -0400

    Removed image carousel

--- client/react/components/about/aboutPage.tsx
+++ client/react/components/about/aboutPage.tsx
@@ -1,15 +1,4 @@
 import React, { memo, useEffect, useState } from 'react';
-import Slider from 'react-slick';
-
-const sliderSettings = {
-    autoplay: true,
-    autoplaySpeed: 4000,
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
 
 const AboutPage = (): JSX.Element => {
     const [heading, setHeading] = useState([]);
@@ -28,32 +17,13 @@ const AboutPage = (): JSX.Element => {
         <div id='main-content'>
             <h1 className='page-title'>{heading}</h1>
             <div className='page-content'>
-            {
-                summary.map((line, index) => (
-                    <p key={index} className='self-summary'>
-                        {line}
-                    </p>
-                ))
-            }
-            <div className='carousel'>
-                <Slider {...sliderSettings}>
-                    <div className='carousel-item'>
-                        <img src='../assets/images/carousel/1.jpg' />
-                    </div>
-                    <div className='carousel-item'>
-                        <img src='../assets/images/carousel/2.jpg' />
-                    </div>
-                    <div className='carousel-item'>
-                        <img src='../assets/images/carousel/3.jpg' />
-                    </div>
-                    <div className='carousel-item'>
-                        <img src='../assets/images/carousel/4.jpg' />
-                    </div>
-                    <div className='carousel-item'>
-                        <img src='../assets/images/carousel/5.jpg' />
-                    </div>
-                </Slider>
-            </div>
+                {
+                    summary.map((line, index) => (
+                        <p key={index} className='self-summary'>
+                            {line}
+                        </p>
+                    ))
+                }
             </div>
         </div>
     );
