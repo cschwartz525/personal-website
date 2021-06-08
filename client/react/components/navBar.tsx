5285822139be10370d3ee818a1aa4fce4ee9a48e
Craig Schwartz <craigschwartz525@gmail.com>
2021-06-07 20:12:28 -0400

    Add link to / on the navbar logo

--- client/react/components/navBar.tsx
+++ client/react/components/navBar.tsx
@@ -1,9 +1,12 @@
 import React, { memo } from 'react';
+import { Link } from 'react-router-dom';
 import NavItem from './navItem';
 
 const NavBar = (): JSX.Element => (
     <div id='navbar'>
-        <img id='navbar-logo' src='assets/images/logo.png' />
+        <Link to='/'>
+            <img id='navbar-logo' src='assets/images/logo.png' />
+        </Link>
         <NavItem
             content='About'
             link='about'
