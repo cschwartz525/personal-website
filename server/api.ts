265291d6a36388dede741ec9af87afd5de76ee9a
Craig Schwartz <craigschwartz525@gmail.com>
2021-09-19 22:25:03 -0400

    First pass at recaptcha

--- server/api.ts
+++ server/api.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Request, Response } from 'express';
 import nodemailer from 'nodemailer';
 import config from './config';
@@ -13,22 +14,38 @@ export const resume = (req: Request, res: Response): void => {
 }
 
 export const sendEmail = (req: Request, res: Response): void => {
-    var transporter = nodemailer.createTransport(config.smtpConfig);
+    var recaptchaResponse = req.body['g-recaptcha-response'];
+    var recaptchaSecret = process.env.RECAPTCHA_SECRET;
+    var recaptchaVerifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${encodeURIComponent(recaptchaSecret)}&response=${encodeURIComponent(recaptchaResponse)}`;
 
-    var mailOptions = {
-        from: req.body.sender,
-        to: config.email_address,
-        subject: 'New email sent from personal website form',
-        text: `Subject: ${req.body.subject}\nFrom: ${req.body.sender}\nMessage: ${req.body.message}`
-    }
+	axios(recaptchaVerifyUrl).then((response) => {
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-            res.write('<h1>Error sending email</h1><a href="/">Back</a>');
-        }
-        console.log('Message sent: ' + info);
-        res.write('<h1>Email sent successfully</h1><a href="/">Back</a>');
+            if (response.data && response.data.success) {
+                var transporter = nodemailer.createTransport(config.smtpConfig);
+
+                var mailOptions = {
+                    from: req.body.sender,
+                    to: config.email_address,
+                    subject: 'New email sent from personal website form',
+                    text: `Subject: ${req.body.subject}\nFrom: ${req.body.sender}\nMessage: ${req.body.message}`
+                };
+
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        console.log(error);
+                        res.write('<h1>Error sending email</h1><a href="/">Back</a>');
+                    }
+                    console.log('Message sent: ' + info);
+                    res.write('<h1>Email sent successfully</h1><a href="/">Back</a>');
+                });
+
+            } else {
+
+                console.log('Recaptcha not verified');
+                res.status(500);
+
+            }
+
     });
 }
 
