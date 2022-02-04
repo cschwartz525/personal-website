3222e0729b5fec06183de449a961c98b0e5f97f6
Craig Schwartz <craigschwartz525@gmail.com>
2022-02-03 22:45:31 -0500

    Add purple

--- client/react/themes/index.ts
+++ client/react/themes/index.ts
@@ -1,7 +1,8 @@
 import blue from './blue';
 import bluegrey from './bluegrey';
 import green from './green';
+import purple from './purple';
 import red from './red';
 import white from './white';
 
-export default { blue, bluegrey, green, red, white } as Record<string, any>;
+export default { blue, bluegrey, green, purple, red, white } as Record<string, any>;
---
+++ client/react/themes/purple.ts
@@ -0,0 +1,17 @@
+import colors from '../styles/colors';
+
+export default {
+    backgroundColor: '#533763',
+    backgroundGradient: 'linear-gradient(45deg, #302050, #775577)',
+    color: colors.white,
+    links: {
+        color: colors.white,
+        hoverColor: colors.orange,
+        visitedColor: colors.orange,
+    },
+    logoUrl: '/assets/images/logo_white.png',
+    nav: {
+        color: colors.white,
+        hoverColor: colors.orange
+    }
+};
